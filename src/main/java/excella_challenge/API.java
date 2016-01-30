cee1e2f5584f0c21538bef8280c148ad5684e06e
rluthar <rishiluthr@gmail.com>
2016-01-30 09:19:11 -0500

    First Commit

---
+++ src/main/java/excella_challenge/API.java
@@ -0,0 +1,29 @@
+package excella_challenge;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+
+public class API {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	@Path(/hellowWorld)
+	public Response helloWorld() {
+		
+	}
+}
